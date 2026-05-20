ce65dad21e4d94c7a4504292d3fa61945fec9978
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2026-05-20 17:22:55 +0600

    update namespace

--- week-1/02.print.cpp
+++ week-1/02.print.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
+using namespace std;
 int main() {
     int x = 10;
     char y = 'A';
-    std::cout << x << std::endl;
-    std::cout << y << std::endl;
+    cout << x << endl;
+    cout << y << endl;
 
 
     // first ask date of birth 
     int day, month, year;
-    std::cout << "Enter your date of birth (day month year): ";
-    std::cin >> day >> month >> year;
-    std::cout << "Your date of birth is: " << day << "/" << month << "/" << year << std::endl;
+    cout << "Enter your date of birth (day month year): ";
+    cin >> day >> month >> year;
+    cout << "Your date of birth is: " << day << "/" << month << "/" << year << endl;
 
     return 0;
 }
